dedb6c815dfa3e6b3b547e2a06a90e18f3c3fe78
imran-it1 <imranhossainsakil22@gmail.com>
2023-09-24 10:58:39 +0600

    Favorite phone card added

--- src/Pages/FavoritePage/FavoritePage.jsx
+++ src/Pages/FavoritePage/FavoritePage.jsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react";
+import FavoritePhoneCard from "./FavoritePhoneCard";
+
 const FavoritePage = () => {
-	return <div>Fvorite Route Content</div>;
+	const [favorites, setFavorites] = useState([]);
+	const [noDataFound, setNoDataFound] = useState(""); // Ekhane Empty array [], false, empty string "" deya jeto
+
+	useEffect(() => {
+		const getFavoritesFromLS = JSON.parse(
+			localStorage.getItem("favorites"),
+		);
+
+		if (getFavoritesFromLS) {
+			setFavorites(getFavoritesFromLS);
+		} else {
+			setNoDataFound("No Data Found");
+		}
+	}, []);
+
+	return (
+		<div className=" max-w-7xl mx-auto my-10">
+			{/* Local Storage a data na thakle */}
+			{noDataFound && (
+				<div className=" h-[80vh] w-full flex justify-center items-center">
+					<h1 className=" text-3xl font-semibold text-pink-600">
+						{noDataFound}
+					</h1>
+				</div>
+			)}
+
+			{favorites && (
+				<div className=" h-[30vh] bg-pink-100 mb-10 rounded">
+					<h1 className=" flex justify-center items-center text-3xl font-medium h-full">
+						Your Favorites
+					</h1>
+				</div>
+			)}
+
+			<div className=" grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+				{favorites?.map((phone) => (
+					<FavoritePhoneCard
+						key={phone.id}
+						phone={phone}
+					></FavoritePhoneCard>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default FavoritePage;
--- src/Pages/FavoritePage/FavoritePhoneCard.jsx
+++ src/Pages/FavoritePage/FavoritePhoneCard.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 
-const PhoneCard = ({ phone }) => {
-	const { id, image, phone_name, brand_name, price, rating } = phone || {};
+const FavoritePhoneCard = ({ phone }) => {
+	const { image, phone_name, brand_name, price } = phone || {};
+	console.log(phone);
 	return (
-		<div>
-			<div className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-				<div className=" relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
-					<img src={image} className="h-full w-full object-cover" />
+		<div className=" mt-10">
+			<div className="flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+				<div className="mx-4 mt-2 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+					<img src={image} className="h-[350px] w-full" />
 				</div>
-				<div className="p-6">
+				<div className="p-2">
 					<div className="mb-2 flex items-center justify-between">
 						<p className="block font-sans text-base font-medium leading-relaxed text-blue-gray-900 antialiased">
 							{brand_name}
@@ -20,14 +21,11 @@ const PhoneCard = ({ phone }) => {
 							$ {price}
 						</p>
 					</div>
-					<p className="block font-sans text-sm font-normal leading-normal text-gray-700 antialiased opacity-75">
-						{rating}
-					</p>
 				</div>
 				<div className="p-6 pt-0">
-					<Link to={`/phones/${id}`}>
+					<Link>
 						<button className="block normal-case w-full select-none rounded-lg bg-pink-200 hover:bg-pink-300 py-3 px-6 text-center align-middle text-sm font-bold text-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85]">
-							See Details
+							Buy Now
 						</button>
 					</Link>
 				</div>
@@ -36,4 +34,4 @@ const PhoneCard = ({ phone }) => {
 	);
 };
 
-export default PhoneCard;
+export default FavoritePhoneCard;
